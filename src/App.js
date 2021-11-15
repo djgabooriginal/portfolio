30e2cc756cd972769979b4e7b97437abdb063b0b
Paola Anaya <p.andrea.a93@gmail.com>
2021-11-14 18:27:32 -0600

    fonts added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Box, Grommet } from 'grommet'
+import { Box, Card, CardHeader, Grommet } from 'grommet'
 import { theme } from './theme'
+
+import './App.css'
 
 function App() {
   return (
     <Grommet theme={theme}>
-      <Box align="center" pad="large" />
+      <Box pad="large" gap="medium" height="large" width="medium">
+        <div className="card-container">
+        <Card >
+          <CardHeader>
+            {'Gabriel Martinez – DJ Gabo'}
+          </CardHeader>
+        </Card>
+        </div>
+      </Box>
     </Grommet>
   )
 }
