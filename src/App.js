77415cddb58e60d176f5f3f35c55437407a4f59f
Paola Anaya <p.andrea.a93@gmail.com>
2021-11-15 02:22:08 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route, HashRouter, Link } from 'react-router-dom'
 import Home from './components/Home'
 import { InfoPage } from './components/Info'
 import { PortfolioPage } from './components/Portfolio'
@@ -7,11 +7,21 @@ import { PortfolioPage } from './components/Portfolio'
 function App() {
   return (
     <HashRouter basename="/">
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/info" element={<InfoPage />} />
           <Route path="/portfolio" element={<PortfolioPage />} />
         </Routes>
+      </div>
     </HashRouter>
   )
 }
